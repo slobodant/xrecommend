06fc97404efd8f37add69d5d581567d374f328b4
slobodant <slobodan.tarbuk@gmail.com>
2013-12-30 17:41:38 +0100

    updated clojure version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject xrecommend "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
 		[org.clojure/clojure-contrib "1.2.0"]
-		[incanter "1.2.3-SNAPSHOT"]]
+		[incanter "1.5.4"]]
 		  :main xrecommend.core)
 
